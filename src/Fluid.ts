801858113d6216440a5d4c7d11a97ad4f79dadf3
SpectralGT <atharvsingh1396@gmail.com>
2022-05-31 12:58:39 +0530

    created add source method

--- src/Fluid.ts
+++ src/Fluid.ts
@@ -1,27 +1,25 @@
 const N = 600;
 
-const XI = (x:number,y:number)=>{
-  return (x + ( (N+2)*y ) );
-}
+const XI = (x: number, y: number) => {
+	return x + (N + 2) * y;
+};
 
 export default class Fluid {
+	size: number = (N + 2) * (N + 2);
+	u: Array<number> = new Array(this.size).fill(0);
+	v: Array<number> = new Array(this.size).fill(0);
+	u_prev: Array<number> = new Array(this.size).fill(0);
+	v_prev: Array<number> = new Array(this.size).fill(0);
+	density: Array<number> = new Array(this.size).fill(0);
+	density_prev: Array<number> = new Array(this.size).fill(0);
 
-  size:number = (N+2)*(N+2);
-  u:Array<number> = new Array(this.size).fill(0);
-  v:Array<number> = new Array(this.size).fill(0);
-  u_prev:Array<number> = new Array(this.size).fill(0);
-  v_prev:Array<number> = new Array(this.size).fill(0);
-  density:Array<number> = new Array(this.size).fill(0);
-  density_prev:Array<number> = new Array(this.size).fill(0);
+	constructor() {}
+
   
-  constructor(){
-    
-  }
-
-  add_source (N:number,x:Array<number>,s:Array<number>,dt:number)
-{
-int i, size=(N+2)*(N+2);
-for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
-}
-
+	addSource(N: number, x: Array<number>, s: Array<number>, dt: number) {
+		var size = (N + 2) * (N + 2);
+		for (var i = 0; i < size; i++) {
+			x[i] += dt * s[i];
+		}
+	}
 }
